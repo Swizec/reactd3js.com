c67a602c6c9cc034914f36ae847da569b41b0d31
Swizec Teller <swizec@swizec.com>
2017-03-12 15:02:40 -0700

    Copy over framework for site making

--- src/App.js
+++ src/App.js
@@ -9,17 +9,15 @@ import './css/bootstrap-custom-utils.css';
 import './css/salesbury-lilac.css';
 //import './css/salesbury-bg-grid_light_wool.css';
 
+import * as Content from './Content';
+
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div>
+                <div className="bg-white-dark padding-small-top" />
+
+                <Content.Header />
             </div>
         );
     }
