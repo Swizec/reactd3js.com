cc8629b357300b67ffb602886ac7a35e3798b626
Swizec Teller <swizec@swizec.com>
2017-03-12 22:57:50 -0700

    Testimonials and tracking codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
@@ -7,12 +6,15 @@ import 'font-awesome/css/font-awesome.css';
 import './css/bootstrap-override.css';
 import './css/bootstrap-custom-utils.css';
 import './css/salesbury-lilac.css';
+import './css/custom.css';
 //import './css/salesbury-bg-grid_light_wool.css';
 
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 OfflinePluginRuntime.install();
 
 import * as Content from './Content';
+import { TestimonialsCarousel } from './Testimonials';
+import { LowSection } from './Section';
 
 
 class App extends Component {
@@ -22,6 +24,12 @@ class App extends Component {
                 <div className="bg-white-dark padding-small-top" />
 
                 <Content.Header />
+
+                <LowSection>
+                    <TestimonialsCarousel keynames={['michel', 'patrick', 'ulterior',
+                                                     'bjorg2', 'myusef',
+                                                     'd3noob']} />
+                </LowSection>
             </div>
         );
     }
