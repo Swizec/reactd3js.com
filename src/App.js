e360b789ae637fee862f1c95679d2e11c80be292
Swizec Teller <swizec@swizec.com>
2017-03-12 23:32:34 -0700

    Some base copy for the page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ OfflinePluginRuntime.install();
 
 import * as Content from './Content';
 import { TestimonialsCarousel } from './Testimonials';
-import { LowSection } from './Section';
+import Section, { LowSection } from './Section';
+import { HalfColumn } from './Columns';
 
 
 class App extends Component {
@@ -29,6 +30,39 @@ class App extends Component {
                     <TestimonialsCarousel keynames={['michel', 'patrick', 'ulterior',
                                                      'bjorg2', 'myusef',
                                                      'd3noob']} />
+                </LowSection>
+
+                <LowSection>
+                    <HalfColumn>
+                        <h2 className="text-center">The book</h2>
+                        <p className="lead">
+                            Learn at your own pace, <b>build cool things</b>, discover the beauty behind <b>declarative data visualization</b> and <b>fun web toys</b>.
+                        </p>
+                        <p>
+                            <a href="https://swizec.com/reactd3js/">
+                                <img src="img/book.png" title="React+D3v4 book"/>
+                            </a>
+                        </p>
+                    </HalfColumn>
+
+                    <HalfColumn>
+                        <h2 className="text-center">The workshop</h2>
+                        <p className="lead">
+                            Learn <b>in person</b> with a <b>group of peers</b>, get <b>detailed walkthroughs</b> for several fun projects, <b>ask anything</b>. We help you solve your specific problem.
+                        </p>
+
+                        <p>&nbsp;</p>
+
+                        <h3>Next workshop: March 25th in San Francisco</h3>
+
+                        <p>
+                            <a href="https://www.eventbrite.com/e/javascript-workshop-reactd3v4-tickets-32708332503">
+                                <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F29174463%2F445634759%2F1%2Foriginal.jpg?w=800&rect=59%2C0%2C1340%2C670&s=0c085262fc7fcda49bd88caca3a3d540" title="React+D3v4 workshop in San Francisco" style={{width: '100%'}} />
+                            </a>
+                        </p>
+
+                        <p className="text-center"><a href="https://www.eventbrite.com/e/javascript-workshop-reactd3v4-tickets-32708332503" className="btn btn-success btn-lg">Get Tickets  👉 </a></p>
+                    </HalfColumn>
                 </LowSection>
             </div>
         );
